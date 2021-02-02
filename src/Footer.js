413db3ba3aa431c77830f3fa7658fed7c118000f
Myriam <myriam.benkiran@gmail.com>
2021-02-02 17:31:10 +0000

    grop limited

--- src/Footer.js
+++ src/Footer.js
@@ -11,7 +11,7 @@ class Footer extends React.Component {
     render() {
         return (
         	<div className="Footer">
-        		<p>2020 Vanilla. All rights reserved.</p>
+        		<p>2021 GROP LIMITED (No. 12425967). All rights reserved.</p>
         		<div className="FooterMenu">
                 		<Link exact to="/about"> About Vanilla </Link>
                     	<Link exact to="/contact"> Contact us </Link>
